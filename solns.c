bb072eb27b5fe8a2880613fb9159166d234d3a31
02qwe <84759939+02qwe@users.noreply.github.com>
2021-06-18 06:35:21 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,9 +1,12 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+#include <limits.h>
 float average(int A[], int size)
-{   for(int i=0;i<size;++i)
-    int sum=A[i];
-    float avg=(float)(sum/size);  
+{   int sum=0;
+    for(int i=0;i<size;++i)
+    sum+=A[i];
+    float avg=(float)(sum/size); 
+    return avg;
 }
 int factors(int size , int A[])
 {
@@ -12,13 +15,19 @@ int factors(int size , int A[])
 }
 int max(int A[], int size)
 {
-  
-  
+  int maxi=intmin;
+  for(int i=0;i<size;++i)
+      if(A[i]>maxi)
+        maxi=A[i];
+ return maxi;
 }
 int min(int A[], int size)
 {
-  
-  
+  int mini=intmax;
+  for(int i=0;i<size;++i)
+      if(A[i]<mini)
+          mini=A[i];
+  return min;
 }
 int mode(int A[], int size )
 {
